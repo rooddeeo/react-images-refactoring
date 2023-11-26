34e8ad8001cbfda686004d30496152b5ed0d303c
rooddeeo <rooddeeo@gmail.com>
2023-11-26 03:21:28 +0200

    React Home Work 4

--- src/api/gallery.js
+++ src/api/gallery.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const getGallery = async (search, page) => {
-  console.log(search);
   const { data } = await axios(
     `/?q=${search}&page=${page}&key=12002814-5debf547df742213b695907de&image_type=photo&orientation=horizontal&per_page=12`
   );
