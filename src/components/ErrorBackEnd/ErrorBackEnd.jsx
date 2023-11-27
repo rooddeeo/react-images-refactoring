01ef3f9e77a67d6ea146bc308dbd4927da5620d5
rooddeeo <rooddeeo@gmail.com>
2023-11-27 09:56:19 +0200

    Update React Home Work 4

--- src/components/ErrorBackEnd/ErrorBackEnd.jsx
+++ src/components/ErrorBackEnd/ErrorBackEnd.jsx
@@ -1,7 +1,11 @@
-import css from "./ErrorBackEnd.module.css"
+import css from './ErrorBackEnd.module.css';
 
 const ErrorBackEnd = ({ errorBackEnd }) => {
-  return <h1 className={css.titleError}>Ups! <span className={css.titleErrorRed}>Error:</span> {errorBackEnd}</h1>;
+  return (
+    <h1 className={css.titleError}>
+      Ups! <span className={css.titleErrorRed}>Error:</span> {errorBackEnd}
+    </h1>
+  );
 };
 
 export default ErrorBackEnd;
